b5a2af870de8e458712c320715e7eda1d7bdd594
supra <alexusenzoroa@gmail.com>
2025-06-11 01:47:42 +0800

    ok never mind, I finally added the label LOL, I just can't seem to place it at the top of the board.

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-// Only for the window
 public class Window extends JFrame {
     public Window() {
         this.setTitle("2048");
@@ -11,6 +10,26 @@ public class Window extends JFrame {
         this.setPreferredSize(new Dimension(1400, 1000));
         this.getContentPane().setBackground(Color.decode("#424242"));
 
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // Instruction label
+        JLabel instructionLabel1 = new JLabel("Use arrow keys to move.");
+
+        instructionLabel1.setForeground(Color.WHITE);
+        instructionLabel1.setFont(new Font("Arial", Font.BOLD, 18));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        this.add(instructionLabel1, gbc);
+
+        JLabel instructionLabel2 = new JLabel("Press U to undo.");
+
+        instructionLabel2.setForeground(Color.WHITE);
+        instructionLabel2.setFont(new Font("Arial", Font.BOLD, 18));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        this.add(instructionLabel2, gbc);
 
         this.pack();
     }
